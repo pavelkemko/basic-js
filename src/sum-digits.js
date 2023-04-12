396d586149a7a34b9310482ba11d2f714f15bdb8
pavelkemko <pavelkemko@gmail.com>
2023-04-12 16:34:54 +0300

    feat: done sum-digits

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,9 +12,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+ let arrNum = n.toString().split('');
+ let count = 0;
+ arrNum.forEach(e => count += +e);
+ if(count > 9) {
+  count = getSumOfDigits(count);
+ }
+ return count;
 }
 
 module.exports = {
